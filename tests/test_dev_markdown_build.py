775d0fb0892da3592f8ebe0d173143a9fcaff7ca
Ryan <ryanrigato@gmail.com>
2019-10-27 18:01:49 -0500

    Modularized the test case

--- tests/test_dev_markdown_build.py
+++ tests/test_dev_markdown_build.py
@@ -113,6 +113,67 @@ class MarkdownLogic(unittest.TestCase):
             self.assertEqual(dir_name[0], "v")
             self.assertTrue(dir_name[1:].isnumeric())
 
+    def check_project_value(self,
+        potential_project, project_name):
+        '''Tests naming conventions for markdown files
+            Parameters
+            ----------
+
+            Returns
+            -------
+
+            Raises
+            ------
+        '''
+        """
+            Testing to make sure the path object is a directory
+            and not hidden/__pycache__ directory
+        """
+        if (os.path.isdir(potential_project)
+            and (project_name[0] not in ['_','.']) ):
+            """
+                dirs gets all directories in
+                the docs folder
+
+                dirs, files will be a list of directories/files
+                in the relative_dir folders
+
+                Will walk over all files in the
+                /docs/v1/project1_name
+                /docs/v1/project2_name
+                /docs/v2/project1_name
+                etc.
+            """
+            for root, dirs, files in os.walk(potential_project):
+
+                logging.info("Project files found: ")
+                logging.info(files)
+                markdown_counter = 0
+
+                """
+                    Iterating over all files
+                    in a project
+                """
+
+                for project_file in files:
+                    """
+                        Gets the markdown file and makes sure it is
+                        named the same as the project folder
+                    """
+
+                    if project_file.split('.')[1].lower() == 'md':
+                        markdown_counter += 1
+                        logging.info("""Ensuring the markdown file
+                        is named the same as the project""")
+                        self.assertEqual(project_name,
+                            project_file.split('.')[0]
+                            )
+
+
+                logging.info("""Making sure we
+                 only have one markdown file per project""")
+                self.assertEqual(markdown_counter, 1 )
+
     def test_markdown_convention(self,
         docs_dir=(WORKING_DIRECTORY + "/docs")):
         '''Tests naming conventions for markdown files
@@ -159,51 +220,10 @@ class MarkdownLogic(unittest.TestCase):
                     docs_dir, ver_name, project_name)
                 logging.info("Project directory: ")
                 logging.info(potential_project)
-                """
-                    Testing to make sure the path object is a directory
-                    and not hidden/__pycache__ directory
-                """
-                if (os.path.isdir(potential_project) and (project_name[0] not in ['_','.']) ):
-                    """
-                        dirs gets all directories in
-                        the docs folder
-
-                        dirs, files will be a list of directories/files
-                        in the relative_dir folders
-
-                        Will walk over all files in the
-                        /docs/v1/project1_name
-                        /docs/v1/project2_name
-                        /docs/v2/project1_name
-                        etc.
-                    """
-                    for root, dirs, files in os.walk(potential_project):
-
-                        logging.info("Project files found: ")
-                        logging.info(files)
-                        markdown_counter = 0
-
-                        """
-                            Iterating over all files
-                            in a project
-                        """
-
-                        for project_file in files:
-                            """
-                                Gets the markdown file and makes sure it is
-                                named the same as the project folder
-                            """
-
-                            if project_file.split('.')[1].lower() == 'md':
-                                markdown_counter += 1
-                                logging.info("Ensuring the markdown file is named the same as the project")
-                                self.assertEqual(project_name,
-                                    project_file.split('.')[0]
-                                    )
-
-
-                        logging.info("Making sure we only have one markdown file per project")
-                        self.assertEqual(markdown_counter, 1 )
+                self.check_project_value(
+                    potential_project=potential_project,
+                    project_name=project_name
+                )
 
 if __name__ == '__main__':
     '''
