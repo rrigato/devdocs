9a48c6ae09cab520c5baa3def1a35bde5fe8550a
Ryan <ryanrigato@gmail.com>
2020-05-23 14:11:04 -0500

    cloudformation update command

--- devops/CI.sh
+++ devops/CI.sh
@@ -1,4 +1,6 @@
 #Update code pipeline
 aws cloudformation update-stack --stack-name devdocs-pipeline \
  --template-body file://templates/code_pipeline.yml \
- --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND
+ --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+ --parameters ParameterKey=ProdCrossAccountArn,\
+ ParameterValue="PROD_CROSS_ACCOUNT_ARN_HERE"
